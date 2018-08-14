83631b66455710695ef2501711051c66130d0abd
Kyle Julian <kjulian3@stanford.edu>
2018-08-13 17:04:07 -0700

    Expanded Julia example

--- readNNet/julia/example.jl
+++ readNNet/julia/example.jl
@@ -1,4 +1,9 @@
 include("nnet.jl")
 
 nnet = NNet("../../nnet/TestNetwork.nnet")
+println("Num Inputs: "*string(num_inputs(nnet)))
+println("Num Outputs: "*string(num_outputs(nnet)))
+println("One evaluation:")
 println(evaluate_network(nnet,[15299.0,-1.142,-1.142,600.0,500.0]))
+println("\nMultiple evaluations at once:")
+println(evaluate_network_multiple(nnet,[[15299.0, -1.142, -1.142, 600.0, 500.0] [15299.0,-1.142,-1.142,600.0,500.0]]))
