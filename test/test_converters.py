2eaa76b2690ecd6167918b0b996ffffea7725cb6
Anshrin Srivastava <Anshrin@gmail.com>
2024-09-27 14:43:20 -0400

    Update test_converters.py

--- test/test_converters.py
+++ test/test_converters.py
@@ -9,8 +9,6 @@ from NNet.converters.pb2nnet import pb2nnet
 from NNet.converters.nnet2pb import nnet2pb
 import onnxruntime
 from NNet.python.nnet import *
-from tensorflow.python.framework import tensor_util
-from tensorflow.python.framework import graph_util
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -22,18 +20,16 @@ class TestConverters(unittest.TestCase):
 
         ### Options###
         nnetFile = "nnet/TestNetwork.nnet"
-        testInput = np.array([1.0,1.0,1.0,100.0,1.0]).astype(np.float32)
+        testInput = np.array([1.0, 1.0, 1.0, 100.0, 1.0]).astype(np.float32)
         ##############
 
         # Convert NNET to ONNX and save ONNX network to given file
-        # Adapt network weights and biases so that no input or output normalization is required to evaluate network
-        onnxFile = nnetFile[:-4]+"onnx"
-        nnet2onnx(nnetFile,onnxFile=onnxFile,normalizeNetwork=True)
+        onnxFile = nnetFile[:-4] + "onnx"
+        nnet2onnx(nnetFile, onnxFile=onnxFile, normalizeNetwork=True)
 
         # Convert ONNX back to NNET and save NNET network
-        # Note that unless input mins and maxes are specified, the minimum and maximum floating point values will be written
-        nnetFile2 = nnetFile[:-4]+"v2.nnet"
-        onnx2nnet(onnxFile,nnetFile=nnetFile2)
+        nnetFile2 = nnetFile[:-4] + "v2.nnet"
+        onnx2nnet(onnxFile, nnetFile=nnetFile2)
 
         ## Test that the networks are equivalent
         # Load models
@@ -44,79 +40,77 @@ class TestConverters(unittest.TestCase):
         # Evaluate ONNX
         onnxInputName = sess.get_inputs()[0].name
         onnxOutputName = sess.get_outputs()[0].name
-        onnxEval = sess.run([onnxOutputName],{onnxInputName: testInput})[0]
+        onnxEval = sess.run([onnxOutputName], {onnxInputName: testInput})[0]
 
         # Evaluate Original NNET
-        inBounds = np.all(testInput>=nnet.mins) and np.all(testInput<=nnet.maxes)
+        inBounds = np.all(testInput >= nnet.mins) and np.all(testInput <= nnet.maxes)
         self.assertTrue(inBounds)
         nnetEval = nnet.evaluate_network(testInput)
 
         # Evaluate New NNET
-        inBounds = np.all(testInput>=nnet2.mins) and np.all(testInput<=nnet2.maxes)
+        inBounds = np.all(testInput >= nnet2.mins) and np.all(testInput <= nnet2.maxes)
         self.assertTrue(inBounds)
         nnetEval2 = nnet2.evaluate_network(testInput)
 
-        percChangeONNX = max(abs((nnetEval-onnxEval)/nnetEval))*100.0
-        percChangeNNet = max(abs((nnetEval-nnetEval2)/nnetEval))*100.0
+        percChangeONNX = max(abs((nnetEval - onnxEval) / nnetEval)) * 100.0
+        percChangeNNet = max(abs((nnetEval - nnetEval2) / nnetEval)) * 100.0
 
         # Evaluation should not change
-        self.assertTrue(percChangeONNX<1e-3)
-        self.assertTrue(percChangeNNet<1e-3)
+        self.assertTrue(percChangeONNX < 1e-3)
+        self.assertTrue(percChangeNNet < 1e-3)
 
     def test_pb(self):
-
         ### Options###
         nnetFile = "nnet/TestNetwork.nnet"
-        testInput = np.array([1.0,1.0,1.0,100.0,1.0]).astype(np.float32)
+        testInput = np.array([1.0, 1.0, 1.0, 100.0, 1.0]).astype(np.float32)
         ##############
 
-        # Convert NNET to tensorflow and save tensorflow network to given file as a frozen protocol buffer file
-        # Adapt network weights and biases so that no input or output normalization is required to evaluate network
-        pbFile = nnetFile[:-4]+"pb"
-        nnet2pb(nnetFile,pbFile=pbFile,normalizeNetwork=True)
+        # Convert NNET to TensorFlow and save the model as a frozen protocol buffer file
+        pbFile = nnetFile[:-4] + "pb"
+        nnet2pb(nnetFile, pbFile=pbFile, normalizeNetwork=True)
 
-        # Convert tensorflow pb back to NNET and save NNET network
-        # Note that unless input mins and maxes are specified, the minimum and maximum floating point values will be written
-        nnetFile2 = nnetFile[:-4]+"v2.nnet"
-        pb2nnet(pbFile,nnetFile=nnetFile2)
+        # Convert TensorFlow pb back to NNET and save the NNET network
+        nnetFile2 = nnetFile[:-4] + "v2.nnet"
+        pb2nnet(pbFile, nnetFile=nnetFile2)
 
         ## Test that the networks are equivalent
         # Load models
         nnet = NNet(nnetFile)
         nnet2 = NNet(nnetFile2)
 
-        ### Read protobuf file and begin session ###
-        with tf.gfile.GFile(pbFile, "rb") as f:
-            graph_def = tf.GraphDef()
+        # Load TensorFlow model from frozen pb
+        with tf.io.gfile.GFile(pbFile, "rb") as f:
+            graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(f.read())
         with tf.Graph().as_default() as graph:
             tf.import_graph_def(graph_def, name="")
-        sess = tf.Session(graph=graph)
+        
+        # Use tf.compat.v1.Session() for execution in TensorFlow 2.x
+        with tf.compat.v1.Session(graph=graph) as sess:
+            placeholders = [x for x in sess.graph.get_operations() if x.node_def.op == 'Placeholder']
+            self.assertTrue(len(placeholders) == 1)
+            inputName = placeholders[0].name
+            outputName = sess.graph.get_operations()[-1].name
 
-        placeholders = [x for x in sess.graph.get_operations() if x.node_def.op == 'Placeholder']
-        self.assertTrue(len(placeholders)==1)
-        inputName = placeholders[0].name
-        outputName = sess.graph.get_operations()[-1].name
+            # Evaluate TensorFlow model
+            pbEval = sess.run(outputName + ":0", {inputName + ":0": testInput.reshape((1, 5))})[0]
 
-        # Evaluate tensorflow
-        pbEval = sess.run(outputName+":0",{inputName+":0": testInput.reshape((1,5))})[0]
+            # Evaluate Original NNET
+            inBounds = np.all(testInput >= nnet.mins) and np.all(testInput <= nnet.maxes)
+            self.assertTrue(inBounds)
+            nnetEval = nnet.evaluate_network(testInput)
 
-        # Evaluate Original NNET
-        inBounds = np.all(testInput>=nnet.mins) and np.all(testInput<=nnet.maxes)
-        self.assertTrue(inBounds)
-        nnetEval = nnet.evaluate_network(testInput)
+            # Evaluate New NNET
+            inBounds = np.all(testInput >= nnet2.mins) and np.all(testInput <= nnet2.maxes)
+            self.assertTrue(inBounds)
+            nnetEval2 = nnet2.evaluate_network(testInput)
 
-        # Evaluate New NNET
-        inBounds = np.all(testInput>=nnet2.mins) and np.all(testInput<=nnet2.maxes)
-        self.assertTrue(inBounds)
-        nnetEval2 = nnet2.evaluate_network(testInput)
+            percChangePB = max(abs((nnetEval - pbEval) / nnetEval)) * 100.0
+            percChangeNNet = max(abs((nnetEval - nnetEval2) / nnetEval)) * 100.0
 
-        percChangePB = max(abs((nnetEval-pbEval)/nnetEval))*100.0
-        percChangeNNet = max(abs((nnetEval-nnetEval2)/nnetEval))*100.0
-
-        # Evaluation should not change
-        self.assertTrue(percChangePB<1e-3)
-        self.assertTrue(percChangeNNet<1e-3)
+            # Evaluation should not change
+            self.assertTrue(percChangePB < 1e-3)
+            self.assertTrue(percChangeNNet < 1e-3)
 
 if __name__ == '__main__':
     unittest.main()
