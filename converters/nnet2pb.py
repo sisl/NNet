d1f764f32d0db9af849ba9e16243d28839ac46ed
Anshrin Srivastava <Anshrin@gmail.com>
2024-09-27 14:45:32 -0400

    Update nnet2pb.py

--- converters/nnet2pb.py
+++ converters/nnet2pb.py
@@ -1,85 +1,85 @@
 import tensorflow as tf
-import numpy as np
+import numpy as np 
 import sys
 from tensorflow.python.framework import graph_util
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from NNet.utils.readNNet import readNNet
 from NNet.utils.normalizeNNet import normalizeNNet
 
-def pb2nnet(pbFile, inputMins=None, inputMaxes=None, means=None, ranges=None, nnetFile="", inputName="", outputName="", savedModel=False, savedModelTags=[]):
+def nnet2pb(nnetFile, pbFile="", output_node_names = "y_out", normalizeNetwork=False):
     '''
-    Write a .nnet file from a frozen Tensorflow protobuf or SavedModel
+    Read a .nnet file and create a frozen Tensorflow graph and save to a .pb file
     
     Args:
-        pbFile (str): If savedModel is false, path to the frozen graph .pb file.
-                      If savedModel is true, path to SavedModel folder, which
-                      contains .pb file and variables subdirectory.
-        inputMins (list): Minimum values for each neural network input.
-        inputMaxes (list): Maximum values for each neural network output.
-        means (list): Mean value for each input and value for mean of all outputs, used for normalization
-        ranges (list): Range value for each input and value for range of all outputs, used for normalization
-        inputName (str, optional): Name of operation corresponding to input. Default: ""
-        outputName (str, optional) Name of operation corresponding to output. Default: ""
-        savedModel (bool, optional) If false, load frozen graph. If true, load SavedModel object. Default: False
-        savedModelTags (list, optional) If loading a SavedModel, the user must specify tags used. Default: []
+        nnetFile (str): A .nnet file to convert to Tensorflow format
+        pbFile (str, optional): Name for the created .pb file. Default: ""
+        output_node_names (str, optional): Name of the final operation in the Tensorflow graph. Default: "y_out"
     '''
-
-    if nnetFile == "":
-        nnetFile = pbFile[:-2] + 'nnet'
-
-    if savedModel:
-        ### Read SavedModel ###
-        sess = tf.compat.v1.Session()
-        tf.compat.v1.saved_model.loader.load(sess, savedModelTags, pbFile)
-
-        ### Simplify graph using outputName, which must be specified for SavedModel ###
-        simp_graph_def = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), [outputName])
-        with tf.Graph().as_default() as graph:
-            tf.import_graph_def(simp_graph_def, name="")
-        sess = tf.compat.v1.Session(graph=graph)
-        ### End reading SavedModel ###
-
+    if normalizeNetwork:
+        weights, biases = normalizeNNet(nnetFile)
     else:
-        ### Read protobuf file and begin session ###
-        with tf.io.gfile.GFile(pbFile, "rb") as f:
-            graph_def = tf.compat.v1.GraphDef()
-            graph_def.ParseFromString(f.read())
-
-        with tf.Graph().as_default() as graph:
-            tf.import_graph_def(graph_def, name="")
-
-        sess = tf.compat.v1.Session(graph=graph)
-
-    ### Retrieve input and output tensors ###
-    inputTensor = sess.graph.get_tensor_by_name(inputName + ":0")
-    outputTensor = sess.graph.get_tensor_by_name(outputName + ":0")
-
-    # Evaluate the graph and save the neural network file
-    # Additional logic to convert the graph to NNET format goes here
+        weights, biases = readNNet(nnetFile)
+    inputSize = weights[0].shape[1]
     
-    # Close session
-    sess.close()
+    # Default pb filename if none are specified
+    if pbFile=="":
+        pbFile = nnetFile[:-4]+'pb'
+    
+    # Reset tensorflow and load a session using only CPUs
+    tf.reset_default_graph()
+    sess = tf.Session()
 
+    # Define model and assign values to tensors
+    currentTensor = tf.placeholder(tf.float32, [None, inputSize],name='input')
+    for i in range(len(weights)):
+        W = tf.get_variable("W%d"%i, shape=weights[i].T.shape)
+        b = tf.get_variable("b%d"%i, shape=biases[i].shape)
+        
+        # Use ReLU for all but last operation, and name last operation to desired name
+        if i!=len(weights)-1:
+            currentTensor = tf.nn.relu(tf.matmul(currentTensor ,W) + b)
+        else:
+            currentTensor =  tf.add(tf.matmul(currentTensor ,W), b,name=output_node_names)
+
+        # Assign values to tensors
+        sess.run(tf.assign(W,weights[i].T))
+        sess.run(tf.assign(b,biases[i]))
+    
+    # Freeze the graph to write the pb file
+    freeze_graph(sess,pbFile,output_node_names)
+    
+def freeze_graph(sess, output_graph_name, output_node_names):
+    '''
+    Given a session with a graph loaded, save only the variables needed for evaluation to a .pb file
+    
+    Args:
+        sess (tf.session): Tensorflow session where graph is defined
+        output_graph_name (str): Name of file for writing frozen graph
+        output_node_names (str): Name of the output operation in the graph, comma separated if there are multiple output operations
+    '''
+    
+    input_graph_def = tf.get_default_graph().as_graph_def()
+    output_graph_def = graph_util.convert_variables_to_constants(
+        sess,                        # The session is used to retrieve the weights
+        input_graph_def,             # The graph_def is used to retrieve the nodes 
+        output_node_names.split(",") # The output node names are used to select the useful nodes
+    ) 
+
+    # Finally we serialize and dump the output graph to the file
+    with tf.gfile.GFile(output_graph_name, "w") as f:
+        f.write(output_graph_def.SerializeToString())
+  
 if __name__ == '__main__':
-    # Read user inputs and run pb2nnet function
-    if len(sys.argv) > 1:
-        pbFile = sys.argv[1]
-        nnetFile = ""
-        inputName = ""
-        outputName = ""
-        savedModel = False
-        savedModelTags = []
-
-        if len(sys.argv) > 2:
-            nnetFile = sys.argv[2]
-        if len(sys.argv) > 3:
-            inputName = sys.argv[3]
-        if len(sys.argv) > 4:
-            outputName = sys.argv[4]
-        if len(sys.argv) > 5:
-            savedModel = bool(sys.argv[5])
-        if len(sys.argv) > 6:
-            savedModelTags = sys.argv[6:]
-
-        pb2nnet(pbFile, nnetFile=nnetFile, inputName=inputName, outputName=outputName, savedModel=savedModel, savedModelTags=savedModelTags)
+    # Read user inputs and run writePB function
+    if len(sys.argv)>1:
+        nnetFile = sys.argv[1]
+        pbFile = ""
+        output_node_names = "y_out"
+        if len(sys.argv)>2:
+            pbFile = sys.argv[2]
+        if len(sys.argv)>3:
+            output_node_names = argv[3]
+        nnet2pb(nnetFile,pbFile,output_node_names)
     else:
-        print("Need to specify a protobuf (.pb) file to convert to NNET format!")
+        print("Need to specify which .nnet file to convert to Tensorflow frozen graph!")
