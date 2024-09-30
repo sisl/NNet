7402ed627431315b72698095bbda6b2cc53122c8
Anshrin Srivastava <Anshrin@gmail.com>
2024-09-29 20:25:24 -0400

    Update test_nnet.py

--- test/test_nnet.py
+++ test/test_nnet.py
@@ -1,8 +1,12 @@
 import unittest
 import sys
 import os
-sys.path.append('..')
 import numpy as np
+
+# Adding the project path to the system path
+sys.path.append('..')
+
+# Import the necessary NNet module
 from NNet.python.nnet import NNet
 
 class TestNNet(unittest.TestCase):
@@ -20,7 +24,7 @@ class TestNNet(unittest.TestCase):
         # Load model
         nnet = NNet(self.nnetFile)
         nnet_eval = nnet.evaluate_network(testInput)
-        
+
         # Assert outputs are almost equal
         np.testing.assert_almost_equal(nnet_eval, expected_output, decimal=7)
 
@@ -28,26 +32,26 @@ class TestNNet(unittest.TestCase):
         testInput = np.array(nnet.mins)
         expected_output_min = np.array([212.91548563, 211.07829431, 212.55109733, 206.67203848, 211.81989652])
         nnet_eval_min = nnet.evaluate_network(testInput)
-        
+
         np.testing.assert_almost_equal(nnet_eval_min, expected_output_min, decimal=7)
 
         # Test if input is below the minimum, output should be same as using min inputs
         testInput_below_min = testInput - 1.0
         nnet_eval_below_min = nnet.evaluate_network(testInput_below_min)
-        
+
         np.testing.assert_almost_equal(nnet_eval_below_min, expected_output_min, decimal=7)
 
         # Test the maximum input point
         testInput = np.array(nnet.maxes)
         expected_output_max = np.array([-0.73022729, 0.43116092, 0.39846494, 0.39557301, 0.38284647])
         nnet_eval_max = nnet.evaluate_network(testInput)
-        
+
         np.testing.assert_almost_equal(nnet_eval_max, expected_output_max, decimal=7)
 
         # Test if input is above the maximum, output should be same as using max inputs
         testInput_above_max = testInput + 1.0
         nnet_eval_above_max = nnet.evaluate_network(testInput_above_max)
-        
+
         np.testing.assert_almost_equal(nnet_eval_above_max, expected_output_max, decimal=7)
 
     def test_evaluate_multiple(self):
@@ -65,26 +69,26 @@ class TestNNet(unittest.TestCase):
         testInput = np.tile(np.array(nnet.mins), (3, 1))
         expected_output_min = np.tile(np.array([212.91548563, 211.07829431, 212.55109733, 206.67203848, 211.81989652]), (3, 1))
         nnet_eval_min = nnet.evaluate_network_multiple(testInput)
-        
+
         np.testing.assert_almost_equal(nnet_eval_min, expected_output_min, decimal=7)
 
         # Test if input is below the minimum, output should be same as using min inputs
         testInput_below_min = testInput - 1.0
         nnet_eval_below_min = nnet.evaluate_network_multiple(testInput_below_min)
-        
+
         np.testing.assert_almost_equal(nnet_eval_below_min, expected_output_min, decimal=7)
 
         # Test the maximum input point
         testInput = np.tile(np.array(nnet.maxes), (3, 1))
         expected_output_max = np.tile(np.array([-0.73022729, 0.43116092, 0.39846494, 0.39557301, 0.38284647]), (3, 1))
         nnet_eval_max = nnet.evaluate_network_multiple(testInput)
-        
+
         np.testing.assert_almost_equal(nnet_eval_max, expected_output_max, decimal=7)
 
         # Test if input is above the maximum, output should be same as using max inputs
         testInput_above_max = testInput + 1.0
         nnet_eval_above_max = nnet.evaluate_network_multiple(testInput_above_max)
-        
+
         np.testing.assert_almost_equal(nnet_eval_above_max, expected_output_max, decimal=7)
 
     def test_num_inputs(self):
