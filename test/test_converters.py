52535416e43153bfbc95bc22202b5bb5a55190a8
Anshrin Srivastava <Anshrin@gmail.com>
2024-09-29 21:38:17 -0400

    Update test_converters.py

--- test/test_converters.py
+++ test/test_converters.py
@@ -1,120 +1,122 @@
-import sys
-import os
 import unittest
+import sys
+sys.path.append('..')
 import numpy as np
 import onnx
-import onnxruntime
-import tensorflow as tf
-
-# Add the root directory to sys.path to ensure we can import NNet modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from NNet.converters.nnet2onnx import nnet2onnx
 from NNet.converters.onnx2nnet import onnx2nnet
 from NNet.converters.pb2nnet import pb2nnet
 from NNet.converters.nnet2pb import nnet2pb
-from NNet.python.nnet import NNet
-
-# Disable TensorFlow GPU-related logs
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import onnxruntime
+from NNet.python.nnet import *
+from tensorflow.python.framework import tensor_util
+from tensorflow.python.framework import graph_util
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import tensorflow as tf
 
 class TestConverters(unittest.TestCase):
 
-    def setUp(self):
-        # Ensure necessary files exist before running tests
-        self.nnetFile = "nnet/TestNetwork.nnet"
-        if not os.path.exists(self.nnetFile):
-            self.skipTest(f"Skipping test: {self.nnetFile} does not exist")
-
     def test_onnx(self):
-        ### Options ###
-        testInput = np.array([1.0, 1.0, 1.0, 100.0, 1.0]).astype(np.float32)
 
-        # Convert NNET to ONNX
-        onnxFile = self.nnetFile[:-4] + ".onnx"
-        nnet2onnx(self.nnetFile, onnxFile=onnxFile, normalizeNetwork=True)
+        ### Options###
+        nnetFile = "nnet/TestNetwork.nnet"
+        testInput = np.array([1.0,1.0,1.0,100.0,1.0]).astype(np.float32)
+        ##############
 
-        # Convert ONNX back to NNET
-        nnetFile2 = self.nnetFile[:-4] + "v2.nnet"
-        onnx2nnet(onnxFile, nnetFile=nnetFile2)
+        # Convert NNET to ONNX and save ONNX network to given file
+        # Adapt network weights and biases so that no input or output normalization is required to evaluate network
+        onnxFile = nnetFile[:-4]+"onnx"
+        nnet2onnx(nnetFile,onnxFile=onnxFile,normalizeNetwork=True)
 
+        # Convert ONNX back to NNET and save NNET network
+        # Note that unless input mins and maxes are specified, the minimum and maximum floating point values will be written
+        nnetFile2 = nnetFile[:-4]+"v2.nnet"
+        onnx2nnet(onnxFile,nnetFile=nnetFile2)
+
+        ## Test that the networks are equivalent
         # Load models
-        nnet = NNet(self.nnetFile)
+        nnet = NNet(nnetFile)
         sess = onnxruntime.InferenceSession(onnxFile)
         nnet2 = NNet(nnetFile2)
 
         # Evaluate ONNX
         onnxInputName = sess.get_inputs()[0].name
         onnxOutputName = sess.get_outputs()[0].name
-        onnxEval = sess.run([onnxOutputName], {onnxInputName: testInput})[0]
+        onnxEval = sess.run([onnxOutputName],{onnxInputName: testInput})[0]
 
         # Evaluate Original NNET
-        self.assertTrue(np.all(testInput >= nnet.mins) and np.all(testInput <= nnet.maxes))
+        inBounds = np.all(testInput>=nnet.mins) and np.all(testInput<=nnet.maxes)
+        self.assertTrue(inBounds)
         nnetEval = nnet.evaluate_network(testInput)
 
         # Evaluate New NNET
-        self.assertTrue(np.all(testInput >= nnet2.mins) and np.all(testInput <= nnet2.maxes))
+        inBounds = np.all(testInput>=nnet2.mins) and np.all(testInput<=nnet2.maxes)
+        self.assertTrue(inBounds)
         nnetEval2 = nnet2.evaluate_network(testInput)
 
-        percChangeONNX = np.max(np.abs((nnetEval - onnxEval) / nnetEval)) * 100.0
-        percChangeNNet = np.max(np.abs((nnetEval - nnetEval2) / nnetEval)) * 100.0
+        percChangeONNX = max(abs((nnetEval-onnxEval)/nnetEval))*100.0
+        percChangeNNet = max(abs((nnetEval-nnetEval2)/nnetEval))*100.0
 
         # Evaluation should not change
-        self.assertTrue(percChangeONNX < 1e-3)
-        self.assertTrue(percChangeNNet < 1e-3)
+        self.assertTrue(percChangeONNX<1e-3)
+        self.assertTrue(percChangeNNet<1e-3)
 
     def test_pb(self):
-        ### Options ###
-        testInput = np.array([1.0, 1.0, 1.0, 100.0, 1.0]).astype(np.float32)
 
-        pbFile = self.nnetFile[:-4] + ".pb"
+        ### Options###
+        nnetFile = "nnet/TestNetwork.nnet"
+        testInput = np.array([1.0,1.0,1.0,100.0,1.0]).astype(np.float32)
+        ##############
 
-        # Check if the .pb file exists, skip the test if not found
-        if not os.path.exists(pbFile):
-            self.skipTest(f"Skipping test because {pbFile} is not found")
+        # Convert NNET to tensorflow and save tensorflow network to given file as a frozen protocol buffer file
+        # Adapt network weights and biases so that no input or output normalization is required to evaluate network
+        pbFile = nnetFile[:-4]+"pb"
+        nnet2pb(nnetFile,pbFile=pbFile,normalizeNetwork=True)
 
-        # Convert NNET to TensorFlow PB
-        nnet2pb(self.nnetFile, pbFile=pbFile, normalizeNetwork=True)
+        # Convert tensorflow pb back to NNET and save NNET network
+        # Note that unless input mins and maxes are specified, the minimum and maximum floating point values will be written
+        nnetFile2 = nnetFile[:-4]+"v2.nnet"
+        pb2nnet(pbFile,nnetFile=nnetFile2)
 
-        # Convert PB back to NNET
-        nnetFile2 = self.nnetFile[:-4] + "v2.nnet"
-        pb2nnet(pbFile, nnetFile=nnetFile2)
-
+        ## Test that the networks are equivalent
         # Load models
-        nnet = NNet(self.nnetFile)
+        nnet = NNet(nnetFile)
         nnet2 = NNet(nnetFile2)
 
-        # Load and evaluate TensorFlow model
-        tf.compat.v1.reset_default_graph()  # Needed if running multiple times in a session
-        graph_def = tf.compat.v1.GraphDef()
-        with tf.io.gfile.GFile(pbFile, "rb") as f:
+        ### Read protobuf file and begin session ###
+        with tf.gfile.GFile(pbFile, "rb") as f:
+            graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
-
         with tf.Graph().as_default() as graph:
             tf.import_graph_def(graph_def, name="")
-        input_tensor = graph.get_tensor_by_name("input:0")
-        output_tensor = graph.get_tensor_by_name("y_out:0")
+        sess = tf.Session(graph=graph)
 
-        # Evaluate using TensorFlow
-        with tf.compat.v1.Session(graph=graph) as sess:
-            tf_eval = sess.run(output_tensor, feed_dict={input_tensor: testInput.reshape(1, -1)})[0]
+        placeholders = [x for x in sess.graph.get_operations() if x.node_def.op == 'Placeholder']
+        self.assertTrue(len(placeholders)==1)
+        inputName = placeholders[0].name
+        outputName = sess.graph.get_operations()[-1].name
+
+        # Evaluate tensorflow
+        pbEval = sess.run(outputName+":0",{inputName+":0": testInput.reshape((1,5))})[0]
 
         # Evaluate Original NNET
-        self.assertTrue(np.all(testInput >= nnet.mins) and np.all(testInput <= nnet.maxes))
+        inBounds = np.all(testInput>=nnet.mins) and np.all(testInput<=nnet.maxes)
+        self.assertTrue(inBounds)
         nnetEval = nnet.evaluate_network(testInput)
 
         # Evaluate New NNET
-        self.assertTrue(np.all(testInput >= nnet2.mins) and np.all(testInput <= nnet2.maxes))
+        inBounds = np.all(testInput>=nnet2.mins) and np.all(testInput<=nnet2.maxes)
+        self.assertTrue(inBounds)
         nnetEval2 = nnet2.evaluate_network(testInput)
 
-        percChangePB = np.max(np.abs((nnetEval - tf_eval) / nnetEval)) * 100.0
-        percChangeNNet = np.max(np.abs((nnetEval - nnetEval2) / nnetEval)) * 100.0
+        percChangePB = max(abs((nnetEval-pbEval)/nnetEval))*100.0
+        percChangeNNet = max(abs((nnetEval-nnetEval2)/nnetEval))*100.0
 
         # Evaluation should not change
-        self.assertTrue(percChangePB < 1e-3)
-        self.assertTrue(percChangeNNet < 1e-3)
-
+        self.assertTrue(percChangePB<1e-3)
+        self.assertTrue(percChangeNNet<1e-3)
 
 if __name__ == '__main__':
     unittest.main()
