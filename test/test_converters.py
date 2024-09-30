13175d1412dc52af88cb739b50071183e0b6c002
Anshrin Srivastava <Anshrin@gmail.com>
2024-09-29 20:28:36 -0400

    Update test_converters.py

--- test/test_converters.py
+++ test/test_converters.py
@@ -5,14 +5,12 @@ import numpy as np
 import tensorflow as tf
 import onnxruntime
 
-# Ensure correct module imports by adjusting the path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# Import converters
-from converters.nnet2onnx import nnet2onnx
-from converters.onnx2nnet import onnx2nnet
-from converters.pb2nnet import pb2nnet
-from converters.nnet2pb import nnet2pb
+# Import necessary modules from NNet
+from NNet.converters.nnet2onnx import nnet2onnx
+from NNet.converters.onnx2nnet import onnx2nnet
+from NNet.converters.pb2nnet import pb2nnet
+from NNet.converters.nnet2pb import nnet2pb
+from NNet.python.nnet import NNet
 
 # Disable TensorFlow GPU-related logs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
