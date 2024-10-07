b1a4d242959d65b464c9e097365b63fae6167e8c
Anshrin Srivastava <Anshrin@gmail.com>
2024-10-07 16:14:41 -0400

    Update test_nnet.py

--- test/test_nnet.py
+++ test/test_nnet.py
@@ -12,38 +12,36 @@ class TestNNet(unittest.TestCase):
     def test_evaluate(self):
         """Test the evaluation of a single input"""
         testInput = np.array([1.0, 1.0, 1.0, 100.0, 1.0], dtype=np.float32)
-        
+
         # Load model
         nnet = NNet(self.nnetFile)
         nnetEval = nnet.evaluate_network(testInput)
         outputVal = np.array([270.94961805, 280.8974763, 274.55254776, 288.10071007, 256.18037737])
-        
-        # Check output within tolerance
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+
+        # Check output using np.testing.assert_allclose for numerical tolerance
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # Test minimum input point
         testInput = np.array(nnet.mins)
         nnetEval = nnet.evaluate_network(testInput)
         outputVal = np.array([212.91548563, 211.07829431, 212.55109733, 206.67203848, 211.81989652])
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # If input is less than the minimum, use the minimum
         testInput -= 1.0
         nnetEval2 = nnet.evaluate_network(testInput)
-        self.assertTrue(np.max(abs(nnetEval2 - outputVal)) < 1e-5)
-        self.assertTrue(np.max(abs(nnetEval2 - nnetEval)) < 1e-5)
+        np.testing.assert_allclose(nnetEval2, outputVal, rtol=1e-5)
 
         # Test maximum input point
         testInput = np.array(nnet.maxes)
         nnetEval = nnet.evaluate_network(testInput)
         outputVal = np.array([-0.73022729, 0.43116092, 0.39846494, 0.39557301, 0.38284647])
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # If input is greater than the maximum, use the maximum
         testInput += 1.0
         nnetEval2 = nnet.evaluate_network(testInput)
-        self.assertTrue(np.max(abs(nnetEval2 - outputVal)) < 1e-5)
-        self.assertTrue(np.max(abs(nnetEval2 - nnetEval)) < 1e-5)
+        np.testing.assert_allclose(nnetEval2, outputVal, rtol=1e-5)
 
     def test_evaluate_multiple(self):
         """Test the evaluation of multiple inputs"""
@@ -53,31 +51,29 @@ class TestNNet(unittest.TestCase):
         nnet = NNet(self.nnetFile)
         nnetEval = np.array(nnet.evaluate_network_multiple(testInput))
         outputVal = np.tile(np.array([270.94961805, 280.8974763, 274.55254776, 288.10071007, 256.18037737]), (3, 1))
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # Test minimum input point
         testInput = np.tile(np.array(nnet.mins), (3, 1))
         nnetEval = nnet.evaluate_network_multiple(testInput)
         outputVal = np.tile(np.array([212.91548563, 211.07829431, 212.55109733, 206.67203848, 211.81989652]), (3, 1))
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # If input is less than the minimum, use the minimum
         testInput -= 1.0
         nnetEval2 = nnet.evaluate_network_multiple(testInput)
-        self.assertTrue(np.max(abs(nnetEval2 - outputVal)) < 1e-5)
-        self.assertTrue(np.max(abs(nnetEval2 - nnetEval)) < 1e-5)
+        np.testing.assert_allclose(nnetEval2, outputVal, rtol=1e-5)
 
         # Test maximum input point
         testInput = np.tile(np.array(nnet.maxes), (3, 1))
         nnetEval = nnet.evaluate_network_multiple(testInput)
         outputVal = np.tile(np.array([-0.73022729, 0.43116092, 0.39846494, 0.39557301, 0.38284647]), (3, 1))
-        self.assertTrue(np.max(abs(nnetEval - outputVal)) < 1e-5)
+        np.testing.assert_allclose(nnetEval, outputVal, rtol=1e-5)
 
         # If input is greater than the maximum, use the maximum
         testInput += 1.0
         nnetEval2 = nnet.evaluate_network_multiple(testInput)
-        self.assertTrue(np.max(abs(nnetEval2 - outputVal)) < 1e-5)
-        self.assertTrue(np.max(abs(nnetEval2 - nnetEval)) < 1e-5)
+        np.testing.assert_allclose(nnetEval2, outputVal, rtol=1e-5)
 
     def test_num_inputs(self):
         """Test the number of inputs"""
