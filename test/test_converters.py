8ad6db8d3a1106ec242e0c16b9a338de70397729
Anshrin Srivastava <Anshrin@gmail.com>
2024-11-10 20:01:32 -0500

    Update test_converters.py

--- test/test_converters.py
+++ test/test_converters.py
@@ -1,7 +1,7 @@
 import unittest 
 import os
 import numpy as np
-import onnxruntime
+import onnxruntime 
 from unittest.mock import patch
 from NNet.converters.nnet2onnx import nnet2onnx
 from NNet.converters.onnx2nnet import onnx2nnet
