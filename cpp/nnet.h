3c09174fab3112edc2d006e7b2ecdffe8b29e540
Anshrin Srivastava <Anshrin@gmail.com>
2024-11-30 13:26:00 -0500

    Update nnet.h

--- cpp/nnet.h
+++ cpp/nnet.h
@@ -1,44 +1,27 @@
 #pragma once
 
-#include <vector>
-#include <memory>
-
-// Neural Network Struct
+//Neural Network Struct
 class NNet {
 public:
-    int numLayers;     // Number of layers in the network
-    int inputSize;     // Number of inputs to the network
-    int outputSize;    // Number of outputs to the network
-    int maxLayerSize;  // Maximum size dimension of a layer in the network
+    int numLayers;     //Number of layers in the network
+    int inputSize;     //Number of inputs to the network
+    int outputSize;    //Number of outputs to the network
+    int maxLayerSize;  //Maximum size dimension of a layer in the network
+    int *layerSizes;   //Array of the dimensions of the layers in the network
 
-    std::vector<int> layerSizes;   // Vector of the dimensions of the layers in the network
-
-    std::vector<double> mins;      // Minimum values of inputs
-    std::vector<double> maxes;     // Maximum values of inputs
-    std::vector<double> means;     // Vector of the means used to scale the inputs and outputs
-    std::vector<double> ranges;    // Vector of the ranges used to scale the inputs and outputs
-
-    // 4D jagged array that stores the weights and biases for the neural network
-    std::vector<std::vector<std::vector<std::vector<double>>>> matrix;
-
-    std::vector<double> inputs;    // Scratch vector for inputs to the different layers
-    std::vector<double> temp;      // Scratch vector for outputs of different layers
-
-    NNet() = default;              // Default constructor
-    ~NNet() = default;             // Default destructor
+    double *mins;      //Minimum value of inputs
+    double *maxes;     //Maximum value of inputs
+    double *means;     //Array of the means used to scale the inputs and outputs
+    double *ranges;    //Array of the ranges used to scale the inputs and outputs
+    double ****matrix; //4D jagged array that stores the weights and biases
+                       //the neural network.
+    double *inputs;    //Scratch array for inputs to the different layers
+    double *temp;      //Scratch array for outputs of different layers
 };
 
-// Functions implemented in C, exposed via C linkage
-extern "C" {
-    std::unique_ptr<NNet> load_network(const char *filename);
-    
-    // Functions marked 'const' where appropriate for immutability
-    int num_inputs(const NNet* network);
-    int num_outputs(const NNet* network);
-
-    // Evaluate network (modifies input and output buffers)
-    int evaluate_network(NNet* network, double *input, double *output, bool normalizeInput, bool normalizeOutput);
-
-    // Frees the network's resources
-    void destroy_network(NNet* network);
-}
+//Functions Implemented
+extern "C" NNet *load_network(const char *filename);
+extern "C" int   num_inputs(void *network);
+extern "C" int   num_outputs(void *network);
+extern "C" int   evaluate_network(void *network, double *input, double *output, bool normalizeInput, bool normalizeOutput);
+extern "C" void  destroy_network(void *network);
